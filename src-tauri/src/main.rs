2059476c8cdd4fa410da9a979bb77cb456c78843
Jakub Barszczewski <jakub.barszczewski@outlook.com>
2021-07-14 14:09:00 +0100

    move commands to separate file and add test

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,14 +3,11 @@
 	windows_subsystem = "windows"
 )]
 
-#[tauri::command]
-fn my_custom_command(invoke_message: String) -> String{
-	format!("Yo! I was invoked from JS, with this message: {}", invoke_message)
-}
+mod commands;
 
 fn main() {
 	tauri::Builder::default()
-	.invoke_handler(tauri::generate_handler![my_custom_command])
+	.invoke_handler(tauri::generate_handler![commands::my_custom_command])
 		.run(tauri::generate_context!())
 		.expect("error while running tauri application");
 }
