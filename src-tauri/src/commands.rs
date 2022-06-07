4516871eed0d8d84e3e69beeba24f1b6e0f3a121
jbarszczewski <jakub.barszczewski@outlook.com>
2022-06-07 11:27:12 +0200

    tauri dependencies update (RC-14)

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,15 +1,21 @@
 #[tauri::command]
-fn my_custom_command(invoke_message: String) -> String{
-	format!("Hey! I was invoked from JS, with this message: {}", invoke_message)
+pub fn my_custom_command(invoke_message: String) -> String {
+  format!(
+    "Hey! I was invoked from JS, with this message: {}",
+    invoke_message
+  )
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+  use super::*;
 
-    #[test]
-    fn it_works() {
-		let result = my_custom_command(String::from("test"));
-        assert_eq!(result, "Hey! I was invoked from JS, with this message: test");
-    }
+  #[test]
+  fn it_works() {
+    let result = my_custom_command(String::from("test"));
+    assert_eq!(
+      result,
+      "Hey! I was invoked from JS, with this message: test"
+    );
+  }
 }
